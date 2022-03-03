b0d961c20b691465211aac0b7cdf08094bef114f
Han Nguyen <hannguyen7796@gmail.com>
2022-03-03 09:00:44 +0700

    first commit

---
+++ JS-array-find-a-value-in-array/JS-array-find-a-value-in-array.js
@@ -0,0 +1,7 @@
+let value = 10;
+let numbers = [-3, 5, 1, 3, 2, 10];
+for (let i = 0; i < numbers.length; i++) {
+    if (value == numbers[i]) {
+        console.log(`Value ${numbers[i]} is found at index ${i}`);
+    }
+}
